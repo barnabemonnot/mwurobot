3f4783cd526c2e7bd30ade38c5bbb2d6fad8da9e
barnabemonnot <barnabe.monnot@yahoo.fr>
2016-09-26 19:07:21 +0800

    Added solution

--- mwuRobot.js
+++ mwuRobot.js
@@ -1,13 +1,32 @@
-var MWURobot = function(N, epsilon) {
+var _ = require("underscore");
 
+var MWURobot = function(N, epsilon) {
+    this.weights = _.range(N).map(function(d) { return 1; });
+    this.epsilon = epsilon;
 }
 
-MWURobot.prototype.receiveCosts = function(c) {
-
+MWURobot.prototype.receiveCosts = function(costs) {
+    for (var i = 0; i < costs.length; i++) {
+        this.weights[i] = this.weights[i] * Math.pow(1-this.epsilon, costs[i]);
+    }
 }
 
 MWURobot.prototype.move = function() {
-
+    // To implement
+    var totalWeight = this.weights.reduce(function(a, b) {
+        return a+b;
+    }, 0);
+    var randomNumber = Math.random() * totalWeight;
+    var move = 0;
+    var tempSum = 0;
+    for (var i = 0; i < this.weights.length; i++) {
+        tempSum += this.weights[i];
+        if (tempSum > randomNumber) {
+            move = i;
+            break;
+        }
+    }
+    return move;
 }
 
 module.exports = MWURobot;
